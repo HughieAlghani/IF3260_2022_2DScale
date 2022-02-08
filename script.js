9086173def83cceb19d280439d0a92704b38ffed
Hughie Alghaniyyu Emiliano <68457533+HughieAlghani@users.noreply.github.com>
2022-02-08 22:21:36 +0700

    WebGL 2D Scale

---
+++ script.js
@@ -0,0 +1,134 @@
+"use strict";
+
+function main() {
+    var canvas = document.querySelector("#canvas");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    var program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
+
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+
+    var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    var colorLocation = gl.getUniformLocation(program, "u_color");
+    var translationLocation = gl.getUniformLocation(program, "u_translation");
+    var rotationLocation = gl.getUniformLocation(program, "u_rotation");
+    var scaleLocation = gl.getUniformLocation(program, "u_scale");
+
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    
+    setGeometry(gl);
+
+    var translation = [0, 0];
+    var rotation = [0, 1];
+    var scale = [1, 1];
+    var color = [Math.random(), Math.random(), Math.random(), 1];
+
+    drawScene();
+
+    webglLessonsUI.setupSlider("#x", {value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
+    webglLessonsUI.setupSlider("#y", {value: translation[1], slide: updatePosition(1), max: gl.canvas.height});
+    webglLessonsUI.setupSlider("#angle", {slide: updateAngle, max: 360});
+    webglLessonsUI.setupSlider("#scaleX", {value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2});
+    webglLessonsUI.setupSlider("#scaleY", {value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2});
+
+    function updatePosition(index) {
+        return function(event, ui) {
+        translation[index] = ui.value;
+        drawScene();
+        };
+    }
+
+    function updateAngle(event, ui) {
+        var angleInDegrees = 360 - ui.value;
+        var angleInRadians = angleInDegrees * Math.PI / 180;
+        rotation[0] = Math.sin(angleInRadians);
+        rotation[1] = Math.cos(angleInRadians);
+        drawScene();
+    }
+
+    function updateScale(index) {
+        return function(event, ui) {
+        scale[index] = ui.value;
+        drawScene();
+        };
+    }
+
+    function drawScene() {
+        webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.useProgram(program);
+
+        gl.enableVertexAttribArray(positionLocation);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        var size = 2;          
+        var type = gl.FLOAT;   
+        var normalize = false; 
+        var stride = 0;        
+        var offset = 0;        
+        gl.vertexAttribPointer(
+            positionLocation, size, type, normalize, stride, offset);
+
+        // set resolusi
+        gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+        // set warna
+        gl.uniform4fv(colorLocation, color);
+
+        // Set translasi
+        gl.uniform2fv(translationLocation, translation);
+
+        // Set rotasi
+        gl.uniform2fv(rotationLocation, rotation);
+
+        // Set scale
+        gl.uniform2fv(scaleLocation, scale);
+
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 18;
+        gl.drawArrays(primitiveType, offset, count);
+    }
+}
+
+function setGeometry(gl) {
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            // kolom kiri
+            0, 0,
+            30, 0,
+            0, 150,
+            0, 150,
+            30, 0,
+            30, 150,
+
+            // baris atas
+            30, 0,
+            100, 0,
+            30, 30,
+            30, 30,
+            100, 0,
+            100, 30,
+
+            // baris tengah
+            30, 60,
+            67, 60,
+            30, 90,
+            30, 90,
+            67, 60,
+            67, 90,
+        ]),
+        gl.STATIC_DRAW);
+}
+
+main();
